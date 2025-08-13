b883ff87b74ec7919eba4f871843e8b8f709cdcb
AIAgentJoffrey <aiagent.joffrey@gmail.com>
2025-08-13 15:34:34 +0300

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.SITE_URL || 'https://toolyard.vercel.app',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
-module.exports = config;
